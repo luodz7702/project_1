088c0bc332d74e4a5a067b54a156c879b1fc4c00
luodz <luodezhi7702@gmail.com>
2021-07-22 22:17:42 +0800

    timer2

--- timer2/js/main.js
+++ timer2/js/main.js
@@ -5,9 +5,14 @@ function showTime() {
 let btnStart = document.querySelector('#start')
 let btnPause = document.querySelector('#pause')
 let timer = null
+let timer1 = null
+let timer2 = null
 btnStart.onclick = function(){
   timer = setInterval(showTime,1000)
+  timer1 = new Date()
 }
 btnPause.onclick = function(){
   clearInterval(timer)
+  timer2 = new Date()
+  document.querySelector('.second').innerText = (timer2 - timer1)/1000
 }
